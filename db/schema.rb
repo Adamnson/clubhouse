124213b1af89e5ea453f4b42b819f206fb106bdc
Adamnson-0dinson <theawakeavatar@gmail.com>
2024-12-16 19:17:07 +0530

    (feat) add views and controller for devise user
    the app allows access to show the posts when users are
    not logged in. The usernames are masked under "Anon"
    the navbar is adapted from bootstrap

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_12_13_183640) do
+ActiveRecord::Schema[7.2].define(version: 2024_12_14_175315) do
   create_table "posts", force: :cascade do |t|
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -28,4 +30,6 @@ ActiveRecord::Schema[7.2].define(version: 2024_12_13_183640) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "posts", "users"
 end
